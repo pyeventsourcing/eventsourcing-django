6cb03da6181590ca50500944f192aebe84e3c07a
John Bywater <john.bywater@appropriatesoftware.net>
2021-08-05 15:47:47 +0100

    Added test for non-interleaving of notification IDs in atomic inserts.

---
+++ tests/test_noninterleaving_notification_ids.py
@@ -0,0 +1,49 @@
+from eventsourcing.persistence import ApplicationRecorder
+from eventsourcing.popo import POPOApplicationRecorder
+from eventsourcing.postgres import (
+    PostgresApplicationRecorder,
+    PostgresDatastore,
+)
+from eventsourcing.sqlite import SQLiteApplicationRecorder, SQLiteDatastore
+from eventsourcing.tests.noninterleaving_notification_ids_testcase import (
+    NonInterleavingNotificationIDsBaseCase,
+)
+from eventsourcing.tests.ramdisk import tmpfile_uris
+from eventsourcing.tests.test_postgres import drop_postgres_table, pg_close_all_connections
+
+from eventsourcing_django.models import StoredEventRecord
+from eventsourcing_django.recorders import DjangoApplicationRecorder
+from tests.test_recorders import DjangoTestCase
+
+
+class TestNonInterleaving(DjangoTestCase, NonInterleavingNotificationIDsBaseCase):
+    db_alias = None
+
+    def create_recorder(self):
+        return DjangoApplicationRecorder(
+            application_name="app", model=StoredEventRecord, using=self.db_alias
+        )
+
+
+class TestNonInterleavingSQLiteFileDB(TestNonInterleaving):
+    insert_num = 1000
+    db_alias = "sqlite_filedb"
+    databases = {"default", "sqlite_filedb"}
+
+
+class TestNonInterleavingPostgres(TestNonInterleaving):
+    insert_num = 10
+    db_alias = "postgres"
+    databases = {"default", "postgres"}
+
+    @classmethod
+    def tearDownClass(cls):
+        # Need to close all connections Django made from other threads,
+        # otherwise Django can't tear down the database.
+        super().tearDownClass()
+        pg_close_all_connections(
+            name="test_eventsourcing_django",
+        )
+
+
+del NonInterleavingNotificationIDsBaseCase
